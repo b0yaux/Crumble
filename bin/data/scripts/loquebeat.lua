344fc4e1ee1316b1ebc41fd26f31a6c01478ff57
jaufre <jaufre.goumet@erg.school>
2026-04-14 04:15:32 +0200

    [pi] Work in progress

--- bin/data/scripts/loquebeat.lua
+++ bin/data/scripts/loquebeat.lua
@@ -20,18 +20,18 @@ local lfo1 = sine(1/16):scale(0,64)
 
 bpm(120)
 -- Create samplers with presentation settings
-local d = delay():on():time(0.375):feedback(0.7):wet(0.8)
-d:connect(amix)
+-- local d = delay():on():time(0.375):feedback(0.7):wet(0.8):connect(amix)
 
 local drum = sampler(seq("s ~ s k k"):fast(2))
-    :connect(d):connect(vmix):blend(1):mix(1):on()
+    :connect(avmix)
+    :blend(1):mix(1):off()
     :position(osc(1/2):shift(0.21))
     :speed(0.67)
     
 
 local clip = s("muzicvids:10"):connect(avmix):blend(1):off()  
 
-local clip = s("rnd:3"):connect(avmix):blend(1):off()   
+local clip = s("rnd:3"):connect(avmix):blend(1):on()   
 
 -- local trvx = sampler("travaux"):connect(avmix):blend(1)
    -- :mix(seq("1 0.3"):fast(lfo1)):on()
